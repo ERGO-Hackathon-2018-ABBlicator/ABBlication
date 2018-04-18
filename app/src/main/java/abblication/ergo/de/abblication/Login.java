a1216d07e9df540205d8e6014f9ead932f0a9be1
Jonas <jonas.schwarzer@online.de>
2018-04-18 16:41:08 +0200

    Functionality of Login

--- app/src/main/java/abblication/ergo/de/abblication/Login.java
+++ app/src/main/java/abblication/ergo/de/abblication/Login.java
@@ -1,48 +1,54 @@
 package abblication.ergo.de.abblication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    public Button login_button;
+    public EditText username;
+    public EditText password ;
+    public TextView error;
+
+    private String username_db = "Jonas";
+    private String password_db = "123";
+    private String username_gui;
+    private String password_gui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_login);
         setSupportActionBar(toolbar);
+
+        login_button = findViewById(R.id.button_login);
+        username = findViewById(R.id.text_username);
+        password = findViewById(R.id.text_password);
+        error = findViewById(R.id.error_message);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                error.setText("");
+                username_gui = String.valueOf(username.getText());
+                password_gui = String.valueOf(password.getText());
+
+                if (username_gui.equals(username_db) && password_gui.equals(password_db)){
+                    Intent Intent = new Intent(view.getContext(), MainActivity.class);
+                    startActivity(Intent);
+                }else{
+                    error.setText("Try again!");
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_abblist) {
-            return true;
-        } else if (id == R.id.action_quiz) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
