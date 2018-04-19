f461cfaf0c02d90efcf77a88bc13abdbc436f3e2
Thomas Cujé <tcuje@freenet.de>
2018-04-19 16:47:53 +0200

    Add apply now button

--- app/src/main/java/abblication/ergo/de/abblication/AbbInfoActivity.java
+++ app/src/main/java/abblication/ergo/de/abblication/AbbInfoActivity.java
@@ -1,13 +1,19 @@
 package abblication.ergo.de.abblication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class AbbInfoActivity extends AppCompatActivity {
+
+    private String abbMail;
+    private String contactMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class AbbInfoActivity extends AppCompatActivity {
             fillText(jsonPosition, R.id.txtStandort, "Standort");
             fillText(jsonPosition, R.id.txtABB, "ABB");
             fillText(jsonPosition, R.id.txtAnsprechpartner, "Ansprechpartner");
+            abbMail = jsonPosition.getString("ABBMail");
+            contactMail = jsonPosition.getString("AnsprechpartnerMail");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -34,4 +42,12 @@ public class AbbInfoActivity extends AppCompatActivity {
         ((TextView) findViewById(txtBeschreibung)).setText(jsonPosition.getString(gruppenbeschreibung));
     }
 
+    public void onApplyNow(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", abbMail, null));
+        intent.putExtra(Intent.EXTRA_CC, new String[]{contactMail});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Bewerbung für Praxisphase");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hallo");
+        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+    }
 }
