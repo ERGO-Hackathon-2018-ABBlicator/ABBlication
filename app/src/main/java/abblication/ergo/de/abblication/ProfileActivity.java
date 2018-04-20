8ff8e2902d2131890fef8388850614b11207d702
Thomas Cujé <tcuje@freenet.de>
2018-04-20 10:05:52 +0200

    Generate profile view dynamically

--- app/src/main/java/abblication/ergo/de/abblication/ProfileActivity.java
+++ app/src/main/java/abblication/ergo/de/abblication/ProfileActivity.java
@@ -1,14 +1,13 @@
 package abblication.ergo.de.abblication;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.ImageView;
 
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.AWSStartupHandler;
@@ -18,44 +17,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import abblication.ergo.de.abblication.model.ABBRow;
-
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String SKILLS = "Skills";
     private static final String STATIONEN = "Stationen";
-    private static final String NAME = "Name";
-
-    private int num_station = 7;
-    private int num_skills = 5;
-    private String[] stations_db = new String[num_station];
-    private String[] skills_db = new String[num_skills];
-    private String[] stations_gui = new String[num_station];
-    private String[] skills_gui = new String[num_skills];
-
-    private List<String> result_skills;
-    private List<String> result_stationen;
-    private String result_firstname;
-    private String result_name;
-
-    private int[] station_id = new int[]{R.id.text_station1, R.id.text_station2, R.id.text_station3, R.id.text_station4,
-            R.id.text_station5, R.id.text_station6, R.id.text_station7};
-    private int[] skill_id = new int[]{R.id.text_skill1, R.id.text_skill2, R.id.text_skill3, R.id.text_skill4, R.id.text_skill5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        result_skills = new ArrayList<>();
-        result_stationen = new ArrayList<>();
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
 
-        // ImageView imageUser = findViewById(R.id.imageUser);
-        // imageUser.setImageBitmap();
+        setContentView(R.layout.activity_profile);
 
         AWSMobileClient.getInstance().initialize(ProfileActivity.this, new AWSStartupHandler() {
             @Override
@@ -71,39 +42,37 @@ public class ProfileActivity extends AppCompatActivity {
                                     JSONObject user = (JSONObject) obj;
 
                                     final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ProfileActivity.this);
-                                    String vNumber = sharedPref.getString("username","");
-                                    if(user.getString("ID").equals(vNumber)){
+                                    String vNumber = sharedPref.getString("username", "");
+                                    if (user.getString("ID").equals(vNumber)) {
 
-                                        result_firstname = user.getString("Vorname");
-                                        result_name = user.getString("Nachname");
+                                        String userFirstname = user.getString("Vorname");
+                                        String userLastname = user.getString("Nachname");
 
-                                        TextView name = findViewById(R.id.firstname_);
-                                        name.setText(result_name + ", " + result_firstname);
+                                        TextView name = findViewById(R.id.fullname);
+                                        name.setText(userLastname + ", " + userFirstname);
 
                                         JSONObject place = (JSONObject) obj;
-                                        JSONArray tagsObj_skills = place.getJSONArray(SKILLS);
-                                        for (int c2 = 0; c2 < tagsObj_skills.length(); c2++) {
-                                            String skill = tagsObj_skills.getString(c2);
-                                            result_skills.add(skill);
-                                        }
+
+                                        LinearLayout stationLayout = findViewById(R.id.layout_stations);
 
                                         JSONArray tagsObj_stationen = place.getJSONArray(STATIONEN);
                                         for (int c2 = 0; c2 < tagsObj_stationen.length(); c2++) {
                                             String station = tagsObj_stationen.getString(c2);
-                                            result_stationen.add(station);
+                                            TextView txtStation = new TextView(ProfileActivity.this);
+                                            txtStation.setText(station);
+                                            txtStation.setPadding(0, 0, 0, 5);
+                                            stationLayout.addView(txtStation);
                                         }
 
-                                        num_skills = Math.min(result_skills.size(), 5);
-                                        num_station = Math.min(result_stationen.size(),7);
+                                        LinearLayout skillsLayout = findViewById(R.id.layout_skills);
 
-                                        for (int i = 0; i < num_station; ++i) {
-                                            TextView text_station1 = findViewById(station_id[i]);
-                                            text_station1.setText(result_stationen.get(i));
-                                        }
-
-                                        for (int i = 0; i < num_skills; ++i) {
-                                            TextView text_skill1 = findViewById(skill_id[i]);
-                                            text_skill1.setText(result_skills.get(i));
+                                        JSONArray skills = place.getJSONArray(SKILLS);
+                                        for (int c2 = 0; c2 < skills.length(); c2++) {
+                                            String skill = skills.getString(c2);
+                                            TextView txtSkill = new TextView(ProfileActivity.this);
+                                            txtSkill.setText(skill);
+                                            txtSkill.setPadding(0, 0, 0, 5);
+                                            skillsLayout.addView(txtSkill);
                                         }
                                     }
                                 }
